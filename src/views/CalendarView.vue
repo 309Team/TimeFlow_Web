0bc049158ee2039cb70dbb6521a150e49c14f93d
Polister <yyla05@foxmail.com>
2023-11-18 11:13:46 +0800

    移除日历

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -1,23 +1,7 @@
 <template>
-    <div>
-        <el-calendar>
-            <!-- 这里使用的是 2.5 slot 语法，对于新项目请使用 2.6 slot 语法-->
-            <template v-slot:dateCell slot-scope="{data}">
-                <p :class="data.isSelected ? 'is-selected' : ''">
-                    {{ data.day.split('-').slice(1).join('-') }} 
-                </p>
-            </template>
-        </el-calendar>
-    </div>
+    <div></div>
 </template>
   
 <script>
 
 </script>
-  
-
-<style>
-.is-selected {
-    color: #1989FA;
-}
-</style>
