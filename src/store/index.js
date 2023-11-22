8a9d48f889948a3b6b0a697fd0f2ec9286cbe25d
Polister <yyla05@foxmail.com>
2023-11-22 23:36:52 +0800

    修复多请求登录过期导致的路由错误的问题

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,12 @@
 import Vue from "vue";
 import Vuex from 'vuex';
 import tab from './tab';
-import calendar from './calendar';
 
 Vue.use(Vuex)
 
 // 创建Vuex实例并导出
 export default new Vuex.Store({
-    modules:{
+    modules: {
         tab,
-        calendar // 日历组件使用
     }
 })
